35d7808220c1a04e706ab41e479494a8d15ba742
ggila <ggilaber@student.42.fr>
2016-02-15 01:35:15 +0100

    macho_header init

---
+++ macho/macho_header.c
@@ -0,0 +1,9 @@
+#include "macho_header.h"
+
+int		main(int ac, char **av)
+{
+//	t_magic	magic;
+
+//	magic_init(&magic);
+	return (0);
+}
---
+++ macho/macho_header.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/07 15:17:27 by ggilaber          #+#    #+#             */
+/*   Updated: 2016/02/13 19:42:17 by ggilaber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACHO_HEADER_H
+# define MACHO_HEADER_H
+
+//# include <ft_printf.h>
+# include <stdint.h>
+
+# ifndef OK
+#  define OK 0
+# endif
+# ifndef KO
+#  define KO 1
+# endif
+
+typedef const uint32_t	t_magic_key;
+
+typedef struct			s_magic_val
+{
+	const char	*const str;
+	const int	i;
+}						t_magic_val;
+
+typedef struct			s_magic
+{
+	t_magic_key	*magic;
+	t_magic_val	*val;
+}						t_magic;
+
+typedef struct			s_magic_data
+{
+	t_magic_key		key;
+	t_magic_val		val;
+}						t_magic_data;
+
+# define DATA_SIZE
+
+static t_magic_data		g_magic[] = {{0xfeedface, {"MH_MAGIC", 0}},
+									{0xcefaedfe, {"MH_CIGAM", 1}},
+									{0xfeedfacf, {"MH_MAGIC_64", 2}},
+									{0xcffaedfe, {"MH_CIGAM_64", 3}},
+									{0xcafebabe, {"FAT_MAGIC", 4}},
+									{0xbebafeca, {"FAT_CIGAM", 5}}};
+
+#endif
