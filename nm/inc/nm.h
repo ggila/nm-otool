5ffbb5b151a3e7f579327bd689b68ad06f920205
ggila <ggilaber@student.42.fr>
2016-02-22 13:03:21 +0100

    init add_ofile

--- nm/inc/nm.h
+++ nm/inc/nm.h
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 12:57:44 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/21 19:23:24 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/02/22 12:46:58 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,17 @@ typedef struct	s_nm_opt
 	char		*b;
 }				t_nm_opt;
 
+typedef struct	s_olist
+{
+	t_ofile			file;
+	strcut s_olist	*next;
+}				t_olist;
+
 typedef struct	s_env
 {
 	t_nm_opt	opt;
+	t_nm_flag	flag;
+	t_olist		*ofile;
 }				t_env;
+
 #endif
--- nm/main.c
+++ nm/main.c
@@ -6,20 +6,54 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 18:43:19 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/21 19:27:27 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/02/22 13:02:11 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 #include "nm.h"
 #include "options.h"
 #include "ft_printf.h"
 
-int	main(int ac, char **av)
+static void	add_ofile(char *file, t_env env)
 {
-	t_env	g_env;
+	static t_olist	**list = &(env->ofile);
+	t_olist			*new;
+
+	if (!(new = malloc(sizeof(t_olist))))
+	{
+		write(1, "malloc() failed\n", 16);
+		return (KO);
+	}
+	if (!(new->file = ft_strdup(file)))
+	{
+		write(1, "ft_strdup() failed\n", 19);
+		return (KO);
+	}
+	new->next = NULL;
+	list = &(new->next);
+}
+
+static int	read_arg(int ac, char **av, t_env *env)
+{
 	int		i;
 
-	if ((i = check_opt(ac, av, &(g_env.opt))) == KO)
+	if ((i = get_opt(ac, av, &(env->opt))) == KO)
+		return (KO);
+	if (i == ac && add_ofile("a.out") == KO)
+		return (KO);
+	while (i < ac)
+		if (add_ofile())
+	return (OK);
+}
+
+int	main(int ac, char **av)
+{
+	t_env	env;
+
+	ft_bzero(env, sizeof(t_env));
+	if (read_arg(ac, av, &env) == KO)
 		return (EXIT_FAILURE);
 	while (i < ac)
 	{
